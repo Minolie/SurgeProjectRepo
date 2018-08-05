cdc1430d6dd5782124c15d2f717a41bac4f194b6
Minolie Fernando <minolifdo@hotmailcom>
2018-08-05 23:52:44 +0530

    Copy deleted

--- BasicUserApplication copy/config/database.js
+++
@@ -1,9 +0,0 @@
-// module.exports = {
-//   database:'mongodb://localhost:27017/login',
-//   secret: 'yoursecret'
-// }
-
-module.exports = {
-    db: 'mongodb://username:password@url:port/db',
-    db_dev: 'mongodb://localhost:27017/login',
-};
